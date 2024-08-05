03ab5c8b1841377c002fae77272035961aa33539
Donald Peters <Donny9585@gmail.com>
2024-08-05 22:00:30 +0000

    Updated config.py

--- amanigo/config.py
+++ amanigo/config.py
@@ -1,7 +1,11 @@
-
 from sqlalchemy import create_engine
 import psycopg2
+from os import getenv
 
+user = getenv('AMANIGO_USER')
+password = getenv('AMANIGO_PWD')
+database = getenv('AMANIGO_DB')
+host = getenv('AMANIGO_HOST')
 
 
 SECRET_KEY = "THTD673&?/YHG/@H393_YEU"
@@ -12,7 +16,7 @@ DEST_IMAGE_PATH = "amanigo/static/images/destinations/"
 PACKAGE_IMAGE_PATH = "amanigo/static/images/packages/"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 SPECIAL_OFFER_IMAGE_PATH = "amanigo/static/images/specialoffers/"
-SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://root@127.0.0.1/amanigotravels"
+SQLALCHEMY_DATABASE_URI=f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
 
 
 MAIL_SERVER='smtp.gmail.com'
